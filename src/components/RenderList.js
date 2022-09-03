49899d15113cb40732036db8ff48d0f97cf62373
DanielGennat <daniel.gennat@freenet.de>
2022-09-03 13:12:33 +0200

    add ListStyling

--- src/components/RenderList.js
+++ src/components/RenderList.js
@@ -12,8 +12,10 @@ import styled from "styled-components";
             {list.map((item) => {
                 return (
                     <Li key={item.id}>
-                        <input type="checkbox" id={item.id}></input>
-                        <label htmlFor={item.id}>{item.name}</label>
+                        <label htmlFor={item.id}>
+                          <input type="checkbox" id={item.id}></input>
+                          {item.name}
+                        </label>
                         <Button>X</Button>
                     </Li>
                 )
@@ -24,6 +26,10 @@ import styled from "styled-components";
 
   const Li = styled.li`
     list-style: none;
+    display: grid;
+    grid-template-columns: 120px 140px;
+    justify-content: center;
+    justify-items: start;
   `;
 
   const Button = styled.button`
